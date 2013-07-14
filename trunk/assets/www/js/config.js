63c5d9e605f9cc5011483a41082ac09306a992c6
migue030@gmail.com <migue030@gmail.com@7e02a476-7029-b8ff-62c7-1501b8d20979>
2013-07-14 22:54:09 +0000

    baseLine final

--- trunk/assets/www/js/config.js
+++ trunk/assets/www/js/config.js
@@ -1,2 +1,12 @@
-IPSERVIDOR = 'http://localhost/~Mike30';//'http://192.168.1.14';
+IPSERVIDOR = 'http://localhost/~Mike30';
+/*
+--Localost
+'http://localhost/~Mike30'
+
+--Ip Gordon
+'http://192.168.1.14';
+
+--Servidor Amazon
+'http://54.235.245.127'
+*/
 SERVICES = '/enjoylifewebservices';
