eaf719726de7e64fa4a847476dd571b2557875a9
jgordon87@gmail.com <jgordon87@gmail.com@7e02a476-7029-b8ff-62c7-1501b8d20979>
2013-08-25 21:12:43 +0000

    cambiosGenerales

--- trunk/assets/www/js/getCurrentPlan.js
+++ trunk/assets/www/js/getCurrentPlan.js
@@ -1,32 +1,84 @@
 $(document).bind('pageinit', function(){
 
-	var jsonObject = $.cookie("userLogged");
+    $('#botonInicioPlan').click(function(){
+        setStartPlan();
+    });
+
+    cargarPlanActual();
+
+});
+
+function cargarPlanActual(){
 
 	$.ajax({
-		url: IPSERVIDOR +'/enjoylifewebservices/plans/getCurrentPlan.php?token='+TOKEN_ID,//aa1c694bf88ef3a00ad53eb030fd528b',
+		url: IPSERVIDOR +'/enjoylifewebservices/plans/getCurrentPlan.php?token=' + TOKEN + '&usuario=' +USUARIO,
 		dataType:"jsonp",
         beforeSend: function() {
             // This callback function will trigger before data is sent
-			$.mobile.loading( 'show', {
-				text: "Cargando plan actual...",
-				textVisible: true,
-				theme: 'a',
-				textonly: false,
-				html: ''
-			});
-        },				
+            $.mobile.loading( "show", {
+                text: "Cargando plan actual",
+                textVisible: true,
+                theme: $.mobile.loader.prototype.options.theme,
+                textonly: false,
+                html: ""
+            });
+        },			
 		success: function(data, status) {
 			var newHtml="";
 			$.each(data.response, function(i,item){ 
 				newHtml = item;
 			});
-			$('#planActualDesc').append(newHtml);
+			if(newHtml == 'noPlan'){
+				$('#botonIniciaPlan').css('visibility','visible');
+			}else{
+				$('#planActualDesc').append(newHtml);
+				$('#botonIniciaPlan').css('visibility','hidden');
+			}
+			
 		},
 		error: function(){
-			alert("There was an error loading the feed");
+			alert("Error de conexión con el servidor, prueba más tarde");
 		},
 		complete: function(){
 			$.mobile.loading('hide');
 		}		
 	});
-});
+
+}
+
+function setStartPlan(){
+	$.ajax({
+		url: IPSERVIDOR +'/enjoylifewebservices/plans/startPlanProgram.php?token=' + TOKEN + '&usuario=' +USUARIO,
+		dataType:"jsonp",
+        beforeSend: function() {
+            // This callback function will trigger before data is sent
+            $.mobile.loading( "show", {
+                text: "Iniciando programa",
+                textVisible: true,
+                theme: $.mobile.loader.prototype.options.theme,
+                textonly: false,
+                html: ""
+            });
+        },			
+		success: function(data, status) {
+		navigator.notification.alert(
+		    'Acabas de empezar con el programa Enjoy Health!',  // message
+		    alertDismissed,         // callback
+		    'Inicio Plan',            // title
+		    'Done'                  // buttonName
+		);			
+			cargarPlanActual();
+			
+		},
+		error: function(){
+			alert("Error de conexión con el servidor, prueba más tarde");
+		},
+		complete: function(){
+			$.mobile.loading('hide');
+		}		
+	});	
+}
+
+function alertDismissed() {
+    // do something
+}
--- trunk/assets/www/js/getWeightTrend.js
+++ trunk/assets/www/js/getWeightTrend.js
@@ -1,28 +1,48 @@
 $(document).ready(function(){
-  var plot2 = $.jqplot ('chart2', [[3,7,9,1,4,6,8,2,5]], {
-      // Give the plot a title.
-      title: 'Tendencia de peso',
-      // You can specify options for all axes on the plot at once with
-      // the axesDefaults object.  Here, we're using a canvas renderer
-      // to draw the axis label which allows rotated text.
-      axesDefaults: {
-        labelRenderer: $.jqplot.CanvasAxisLabelRenderer
-      },
-      // An axes object holds options for all axes.
-      // Allowable axes are xaxis, x2axis, yaxis, y2axis, y3axis, ...
-      // Up to 9 y axes are supported.
-      axes: {
-        // options for each axis are specified in seperate option objects.
-        xaxis: {
-          label: "Semana",
-          // Turn off "padding".  This will allow data point to lie on the
-          // edges of the grid.  Default padding is 1.2 and will keep all
-          // points inside the bounds of the grid.
-          pad: 0
+      var arreglosPeso = [];
+      $.ajax({
+        url: IPSERVIDOR + '/enjoylifewebservices/history/getWeightTrend.php?token=aa1c694bf88ef3a00ad53eb030fd528b&callback=a&usuario='+USUARIO,
+        dataType:"jsonp",
+        success: function(data, status) {
+
+          $.each(data.response, function(i,item){ 
+            var arregloPeso = [];
+
+            arregloPeso.push(item['fecha']);
+            arregloPeso.push(item['peso']);
+            
+            arreglosPeso.push(arregloPeso);
+          });
         },
-        yaxis: {
-          label: "Peso"
+        error: function(){
+          alert("There was an error loading the feed");
+        },
+        complete: function(){
+
+          //PIE
+          var plot2 = $.jqplot ('chart2', [arreglosPeso], {
+              // Give the plot a title.
+              title: 'Tendencia de peso',
+              // You can specify options for all axes on the plot at once with
+              // the axesDefaults object.  Here, we're using a canvas renderer
+              // to draw the axis label which allows rotated text.
+
+              // An axes object holds options for all axes.
+              // Allowable axes are xaxis, x2axis, yaxis, y2axis, y3axis, ...
+              // Up to 9 y axes are supported.
+              axes: {
+                // options for each axis are specified in seperate option objects.
+                xaxis:{
+                  renderer:$.jqplot.DateAxisRenderer
+                },
+                yaxis: {
+                  label: "Peso"
+                }
+              }
+            });
+
+          $.mobile.loading('hide');
         }
-      }
-    });
+      });
+
 });
